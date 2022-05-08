9dfa6e2354ecaf6b42972638c512b05c971ae781
Carlos Henrique <chsjr1996@gmail.com>
2022-05-08 08:50:28 -0300

    feat: add autocommand to set HTML syntax on Twig files

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,6 +1,7 @@
 lvim.autocommands.custom_groups = {
   -- { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
   { "BufWinEnter", "*.php", "setlocal ts=4 sw=4" },
+  { "BufWinEnter", "*.twig", "set syntax=html" },
   { "BufEnter", "*", ":lua require('user.zen').toggle_ui()" },
   { "InsertEnter", "*", ":normal zz" },
 }
