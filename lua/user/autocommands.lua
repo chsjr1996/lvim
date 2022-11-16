0434d904211a80600473d6cd03a7daf7cd49f6cf
Carlos Henrique <chsjr1996@gmail.com>
2022-11-15 21:01:50 -0300

    feat: fix autocommands (LunarVim upgrade)

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,7 +1,18 @@
-lvim.autocommands.custom_groups = {
-  -- { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
-  { "BufWinEnter", "*.php", "setlocal ts=4 sw=4" },
-  { "BufEnter", "*", ":lua require('user.zen').toggle_ui()" },
-  { "InsertEnter", "*", ":normal zz" },
+lvim.autocommands = {
+  {
+    "BufWinEnter",
+    {
+      pattern = { "*.php" },
+      command = "setlocal ts=4 sw=4"
+    }
+  },
+  {
+    "BufWinEnter",
+    {
+      pattern = { "*.twig" },
+      command = "set syntax=html"
+    }
+  }
+  --   { "InsertEnter", "*", ":normal zz" },
 }
 
